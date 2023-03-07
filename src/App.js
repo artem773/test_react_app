3a1d95d278b0cc7df64475c30b508fd8e0519bca
artem <artem140742@gmail.com>
2023-03-07 22:30:43 +0200

    add posts to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import ClassCounter from "./components/classCounter";
 import Counter from "./components/counter";
+import './styles/app.css';
+import PostItem from "./components/postItem";
+import PostsList from "./components/postsList";
 
 function App() {  
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript_1', body: 'Descr'},
+        {id: 2, title: 'JavaScript_2', body: 'Descr'},
+        {id: 3, title: 'JavaScript_3', body: 'Descr'},
+    ]);
+    const [posts2, setPosts2] = useState([
+      {id: 1, title: 'Python_1', body: 'Descr'},
+      {id: 2, title: 'Python_2', body: 'Descr'},
+      {id: 3, title: 'Python_3', body: 'Descr'},
+  ]);
 
-    const [value , setValue] = useState('text');
 
   return (
     <div className="App">
-       <ClassCounter/>
-    </div>
+        <PostsList posts={posts} title="Posts about JS"/>
+        <PostsList posts={posts2} title="Posts about Python"/>
+    </div> 
   )
 }
 
